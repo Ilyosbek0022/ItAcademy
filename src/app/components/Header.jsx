826cab6a84793a831a64ab10fa5486fe0182f7c0
Ilyos Janoblari <ilyosbeknuriddinov14@gmail.com>
2026-03-12 16:36:32 +0500

    Changes to be committed:

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import PrimaryButton from './buttons/btnprim'
+import Image from 'next/image'
 
 const Header = () => {
     
   return (
     <div>
          <header >
-<div className="text text-white text-3xl font-bold">It <span className="text-[#00e5ff]">Academy</span></div>
+<div className="text text-white text-3xl font-bold"><Image src={"/vercel.svg"} height={60} width={120} alt='logo'/></div>
 <div className="links flex gap-5">
   <a href="#">Home</a>
   <a href="#">Courses</a>
